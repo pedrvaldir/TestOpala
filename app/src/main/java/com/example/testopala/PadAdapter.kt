e064471c8c6441341e99ddf1b19b1a18e1a9ce7c
Valdir <pedrvaldir@gmail.com>
2019-06-14 21:23:38 -0300

    implement PadAdapter receiving list

---
+++ app/src/main/java/com/example/testopala/PadAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.testopala
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.pad_entry.view.*
+
+class PadAdapter(val padList: List<String>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        
+        val pad = this.padList[position]
+
+        val context = parent?.context
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.pad_entry, parent, false)
+        view.id_pad.text = pad!!
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return padList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return padList.size
+    }
+}
